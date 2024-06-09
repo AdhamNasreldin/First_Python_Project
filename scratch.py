b008ed83c789e6a50bda221827e256cbdffdf29e
Adham Nasreldin <adhamnasreldin16@gmail.com>
2024-06-09 20:52:58 +0300

    adding the file to github repo

---
+++ scratch.py
@@ -0,0 +1,12 @@
+age = 20
+age =30
+price =19.95
+is_online = False
+is_offline = True
+print (price)
+print(age)
+
+Name= "John Smith"
+Age = 20
+name = input("What is your name? ")
+print ("Hello " + name + "!")
